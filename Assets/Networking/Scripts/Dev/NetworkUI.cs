e0e0a751d38d82d67c97cc22610b8f6e71cec772
LunarHackett003 <LunarHackett003@users.noreply.github.com>
2023-08-27 20:35:56 +0100

    Add Input System and Quantum Console

--- Assets/Networking/Scripts/Dev/NetworkUI.cs
+++ Assets/Networking/Scripts/Dev/NetworkUI.cs
@@ -1,18 +1,25 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.Netcode;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class NetworkUI : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    [SerializeField] protected Button clientButton, hostButton;
+    private void Awake()
     {
-        
+        if(clientButton)
+        clientButton.onClick.AddListener(() =>
+        {
+            NetworkManager.Singleton.StartClient();
+            
+        });
+        if(hostButton)
+        hostButton.onClick.AddListener(() =>
+        {
+            NetworkManager.Singleton.StartHost();
+        });
     }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+    
 }
