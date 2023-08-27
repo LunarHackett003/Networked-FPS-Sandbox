e8bebbb42dd3bf9250e0a49ac1aab3804b6d5b2c
LunarHackett003 <LunarHackett003@users.noreply.github.com>
2023-08-27 21:57:43 +0100

    Fixed some input things

--- Assets/Scripts/PlayerInputManager.cs
+++ Assets/Scripts/PlayerInputManager.cs
@@ -29,50 +29,55 @@ namespace Eclipse.Input
         protected Controls controls;
         private void OnEnable()
         {
+            if (!IsOwner)
+                return;
             controls = new();
             controls.Enable();
+            GetComponentInChildren<Camera>().gameObject.SetActive(IsOwner);
+            
         }
         private void OnDisable()
         {
+            if (!IsOwner)
+                return;
             controls.Disable();
             controls.Dispose();
         }
 
         private void Update()
         {
-            if (IsOwner)
+            if (!IsOwner)
+                return;
+            inMenu = QuantumConsole.Instance.transform.GetChild(0).gameObject.activeInHierarchy;
+            if (!inMenu)
             {
-                inMenu = QuantumConsole.Instance.transform.GetChild(0).gameObject.activeInHierarchy;
-
-                if (!inMenu)
-                {
-                    moveInput = controls.WorldInput.Move.ReadValue<Vector2>();
-                    lookInput = controls.WorldInput.Aim.ReadValue<Vector2>();
-                }
-                else
-                {
-                    moveInput = Vector2.zero;
-                    lookInput = Vector2.zero;
-                }
+                moveInput = controls.WorldInput.Move.ReadValue<Vector2>();
+                lookInput = controls.WorldInput.Aim.ReadValue<Vector2>();
+            }
+            else
+            {
+                moveInput = Vector2.zero;
+                lookInput = Vector2.zero;
             }
         }
         private void FixedUpdate()
         {
-            if (IsOwner)
-            {
-                if (head)
-                {
-                    headAngle += -lookInput.y * lookSpeed * Time.fixedDeltaTime;
-                    headAngle = Mathf.Clamp(headAngle, -89.5f, 89.5f);
-                    head.transform.localEulerAngles = Vector3.right * headAngle;
-                }
-                else
-                {
-                    head.transform.localEulerAngles = Vector3.zero;
-                }
-                transform.Rotate(transform.up * lookSpeed * lookInput.x * Time.fixedDeltaTime);
-                transform.Translate(new Vector3(moveInput.x, 0, moveInput.y) * Time.fixedDeltaTime * moveSpeed, Space.Self);
-            }
+            if (!IsOwner)
+                return;
+
+             if (head)
+             {
+                 headAngle += -lookInput.y * lookSpeed * Time.fixedDeltaTime;
+                 headAngle = Mathf.Clamp(headAngle, -89.5f, 89.5f);
+                 head.transform.localEulerAngles = Vector3.right * headAngle;
+             }
+             else
+             {
+                 head.transform.localEulerAngles = Vector3.zero;
+             }
+             transform.Rotate(transform.up * lookSpeed * lookInput.x * Time.fixedDeltaTime);
+             transform.Translate(new Vector3(moveInput.x, 0, moveInput.y) * Time.fixedDeltaTime * moveSpeed, Space.Self);
+            
         }
     }
 }
